86a76b1ce6846148ced3b6e9476cb32d8e3291c5
System Administrator <root@Lapchopper.home>
2014-11-29 19:27:17 -0500

    iNew scripts

---
+++ bin/keystrokes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import sys
+from AppKit import NSApplication, NSApp
+from Foundation import NSObject, NSLog
+from Cocoa import NSEvent, NSKeyDownMask
+from PyObjCTools import AppHelper
+
+class AppDelegate(NSObject):
+    def applicationDidFinishLaunching_(self, notification):
+        mask = NSKeyDownMask
+        NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(mask, handler)
+
+def handler(event):
+    try:
+        NSLog(u"%@", event)
+    except KeyboardInterrupt:
+        AppHelper.stopEventLoop()
+
+def main():
+    app = NSApplication.sharedApplication()
+    delegate = AppDelegate.alloc().init()
+    NSApp().setDelegate_(delegate)
+    AppHelper.runEventLoop()
+
+if __name__ == '__main__':
+    main()

